8a6e2cd4e4aa27418c189c11d328dd974f7b1b9d
25Js <js3773882@gmail.com>
2026-02-18 00:01:25 +0530

    .

---
+++ spring/core java/notes.java
@@ -0,0 +1,16 @@
+/// javacc = java compiler 
+/// jvm understand byte code
+/// jre = jvm+ libraries
+/// byte classs= j.class file
+/// javac class= j.java file
+/// javaa id Write once snd read everywhere
+/// 
+/// data types
+/// default will be double in case of foalt/double
+/// use f character like 5.6f to make it float /// /// type casting /// implicit type casting = automatic type conversion /// explicit type casting = manual type conversion /// /// 
+/// it work with unicode character set of two byte size
+/// for long also wee need to add l character like 1234l 
+/// for working with binary we can use 0b or 0B like 0b1010 will give value of 10
+/// same for hexadecimal we can use 0x or 0X like 0x1A will give value of 26
+/// / for octal we can use 0 like 012 will give value of 10
+///  
